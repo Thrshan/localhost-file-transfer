ade3540571cccc2d0773a2af3a45a12c183afbc6
Thrshan <thrshan.jeevaraj@gmail.com>
2021-03-24 13:21:02 +0530

    First commit - Add files to repository

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const multer  = require('multer')
+
+var app = express()
+const port = 3000;
+
+app.use(express.static('public'));
+ 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'D:/Project/Website/file-share/uploads')
+    },
+    filename: function (req, file, cb) {
+    //   cb(null, file.fieldname + '-' + Date.now())
+      cb(null, file.originalname)
+    }
+  });
+const upload = multer({ storage: storage })
+// const upload = multer({ dest: 'uploads/' })
+
+app.post('/sendFile', upload.single('fieldname'), function (req, res, next) {
+    console.log(req.file);
+    res.redirect('/');
+});
+ 
+app.get("/", function (req, res){
+    res.sendFile('index.html');
+})
+
+app.listen(port, function (){
+    console.log("Server started at port " + port);
+});
